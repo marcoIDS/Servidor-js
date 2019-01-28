c31149a8e152388ca20583c6bdc977c8e7aa86bf
Marco <163167@ids.upchiapas.edu.mx>
2019-01-27 22:11:08 -0600

    Version2

--- Servidor.js
+++ Servidor.js
@@ -1,33 +1,49 @@
+console.log("Hola Node")
+var express = require('express')
+var app = express()
+
+var server = require('http').Server(app)
+var io = require('socket.io')(server)
 //var os = require('os')
-const net = require('net')
-const express = require('express')
-let app = express()
+//var net = require('net')
 
-let server = require('http').Server(app)
-let io = require('socket.io')(server)
-const HOST = "192.168.0.12"
-const PORT = "3000"
+//var interfaces = os.networkInterfaces();
+//var addresses = [];
 
+/*for(var k in interfaces){
+    for(var k2 in interfaces[k]){
+        var address = interfaces[k][k2];
+        if(address.family == "IPv4" && !address.internal){
+            console.log(address.address);
+            addresses.push(address.address);
+        }
+    }
+}*/
 
-io.on('connection', function(socket){
-    console.log("Conectado" + socket)
+//var HOST =  String(address.address);//'192.168.13.161'
+var PORT = process.env.PORT || 3000;
+app.get("/",function(reg,res){
+    res.send("Welcome to NodeJS App on Heroku")
+})
+app.use(express.static('client'))
+//var sockets = [];
+io.on('connection',function(socket){
+    console.log("Conectado: "+ socket )
 })
 
-/*
 server.listen(PORT, function(){
-    console.log("Servidor Activo" + PORT)
+    console.log("Servidor activo: " + PORT)
 })
-*/
 
-net.createServer(function(sock){
-    console.log("Conectado" + sock.remoteAddress + ":" + sock.remotePort)
-    sock.on("data", function(data){
+/*net.createServer(function(sock){ //Funcion principal que contiene subFunciones
+    console.log("Conectado Servidor creado"+ sock.remoteAddress + ':' + sock.remotePort)
+    sock.on('data' ,function(data){
         console.log(data)
     })
-
     sock.on('close', function(){
-        console.log('desconectado')
+        console.log("Desconectado")
     })
+}).listen(PORT,HOST)*/
 
-}).listen(PORT, HOST)
+
 
